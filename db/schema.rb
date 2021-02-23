91f75df57fd655ff42e541bad892cb611c473d3e
takahashishu <st1323.www.yoru@gmail.com>
2021-02-23 08:43:57 +0000

    勤怠Aスタート

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20201220080354) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2021-01-30 23:00:00"
-    t.datetime "work_time", default: "2021-01-30 22:30:00"
+    t.datetime "basic_time", default: "2021-02-22 23:00:00"
+    t.datetime "work_time", default: "2021-02-22 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
