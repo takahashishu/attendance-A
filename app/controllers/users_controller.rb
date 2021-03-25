972f2207217bf8d615c368b8aa41f10b15492cf6
takahashishu <st1323.www.yoru@gmail.com>
2021-03-25 22:37:24 +0000

    上長属性追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :department, :employee_number, :card_ID, :designated_work_start_time, :designated_work_end_time, :password, :password_confirmation)
     end
     
     def basic_info_params
